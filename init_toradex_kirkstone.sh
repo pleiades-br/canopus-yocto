f314aa4e5a261f5a1830815d741007e3154d5bc6
Aluisio (ZyZy) Victal <alvictal@gmail.com>
2024-06-10 13:57:29 +0000

    feat: Adding a explication inside init_toradex script

--- init_toradex_kirkstone.sh
+++ init_toradex_kirkstone.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Create compilation dir and download Toradex source code
 mkdir ${HOME}/oe-core
 cd ${HOME}/oe-core
 repo init -u git://git.toradex.com/toradex-manifest.git -b kirkstone-6.x.y -m tdxref/default.xml
